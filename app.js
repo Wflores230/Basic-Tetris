03615b66bd62e288d06e69b282b426bdb4811081
William Flores <floreswilliam891@hotmail.com>
2022-03-20 20:33:28 -0400

    adding button start and pause

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
-    const ScoreDisplay = document.querySelector('#score')
-    const StartBtn = document.querySelector('#start-button')
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
     const width = 10
     let nextRandom = 0
+    let timerId
 
     const lTetromino = [
         [1, width+1, width*2+1, 2],
@@ -67,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     //make the tetrominos move down every second
-    timerId = setInterval(moveDown, 500)
+    //timerId = setInterval(moveDown, 500)
 
     //assign functions to keyCodes
     function control(e) {
@@ -162,4 +163,17 @@ document.addEventListener('DOMContentLoaded', () => {
             displaySquares[displayIndex + index].classList.add('tetromino')
         })
     }
+
+    //adding button
+    startBtn.addEventListener('click', () => {
+        if(timerId) {
+            clearInterval(timerId)
+            timerId = null
+        } else {
+            draw()
+            timerId = setInterval(moveDown, 500)
+            nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+            displayShape()
+        }
+    })
 });
